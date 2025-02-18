87e981b998aab9bc0a0e6f2dcfea3b7b8ce786bf
Alex Johnson <johnsonav1992@gmail.com>
2025-02-17 19:53:57 -0600

    add redirect from root path to welcome screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { WelcomeScreenComponent } from './views/welcome-screen/welcome-screen.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   {
     path: 'welcome',
     component: WelcomeScreenComponent,
