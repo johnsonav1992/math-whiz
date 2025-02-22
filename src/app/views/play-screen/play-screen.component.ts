1a44d40c9d5fc3508c82e7d9795667c263d19a7d
Alex Johnson <johnsonav1992@gmail.com>
2025-02-22 14:38:59 -0600

    implement play screen logic with dynamic color blocks and random operands

--- src/app/views/play-screen/play-screen.component.ts
+++ src/app/views/play-screen/play-screen.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { ColorBlockComponent } from '../../components/color-block/color-block.component';
+import { colorBlocksMap } from '../../../styles/colorBlockDefs';
 
 @Component({
   selector: 'play-screen',
-  imports: [],
+  imports: [ColorBlockComponent],
   templateUrl: './play-screen.component.html',
   styleUrl: './play-screen.component.scss'
 })
-export class PlayScreenComponent {
+export class PlayScreenComponent implements OnInit {
+  colorBlocksMap = colorBlocksMap;
 
+  operandOne = signal(0);
+  operandTwo = signal(0);
+
+  operandOneBlocks = computed(() =>
+    Array.from({ length: this.operandOne() }, () => Math.random())
+  );
+  operandTwoBlocks = computed(() =>
+    Array.from({ length: this.operandOne() }, () => Math.random())
+  );
+
+  ngOnInit() {
+    this.operandOne = signal(Math.floor(Math.random() * 10));
+    this.operandTwo = signal(Math.floor(Math.random() * 10));
+  }
 }
---
+++ src/styles/colorBlockDefs.ts
@@ -0,0 +1,25 @@
+export const blockColorDefs = {
+  red: '#e7161e',
+  orange: '#fe9727',
+  yellow: '#e1cb1f',
+  green: '#4aec04',
+  blue: '#31ecff',
+  purple: '#5629b7',
+  sevenPurple: '#be6edb',
+  pink: '#e02897',
+  gray: '#aba9aa',
+  white: '#fff'
+};
+
+export const colorBlocksMap: Record<number, string> = {
+  1: blockColorDefs.red,
+  2: blockColorDefs.orange,
+  3: blockColorDefs.yellow,
+  4: blockColorDefs.green,
+  5: blockColorDefs.blue,
+  6: blockColorDefs.purple,
+  7: blockColorDefs.sevenPurple,
+  8: blockColorDefs.pink,
+  9: blockColorDefs.gray,
+  10: blockColorDefs.white
+};
