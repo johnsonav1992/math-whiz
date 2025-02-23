464b5de019e2041f659b9ca8cafda502af92d5da
Alex Johnson <johnsonav1992@gmail.com>
2025-02-23 06:05:11 -0600

    refactor blocks-group and play-screen components to handle zero operand logic and adjust styling accordingly

--- src/app/components/blocks-group/blocks-group.component.ts
+++ src/app/components/blocks-group/blocks-group.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  input
+} from '@angular/core';
 import { colorBlocksMap } from '../../../styles/colorBlockDefs';
 import { ColorBlockComponent } from '../color-block/color-block.component';
 
@@ -9,11 +16,23 @@ import { ColorBlockComponent } from '../color-block/color-block.component';
   styleUrl: './blocks-group.component.scss'
 })
 export class BlocksGroupComponent {
+  private _el = inject(ElementRef);
+
   colorBlocksMap = colorBlocksMap;
+  public hostEl = this._el.nativeElement as HTMLDivElement;
 
   operand = input.required<number>();
+  isOneOfTheOperandsZero = input.required<boolean>();
 
   operandBlocks = computed(() =>
     Array.from({ length: this.operand() }, () => Math.random())
   );
+
+  _ = effect(() => {
+    if (this.isOneOfTheOperandsZero()) {
+      this.hostEl.style.margin = 'auto 0';
+    } else {
+      this.hostEl.style.margin = '0';
+    }
+  });
 }
